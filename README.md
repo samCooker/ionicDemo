c2288e048e845cc46967dbf27ed9c5e5c36f0679
samCooker <984616868@qq.com>
2015-12-02 23:41:28 +0800

    新添加注册弹窗

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ionicDemo
 ionic demo template
+*****关于图标*****
+访问http://ionicons.com/  可查询具体图标名称
+或 查看ionicons-2.0.1.zip
+*****************
+
+*******弹出窗demo可查看首页的注册对话框**********
--- www/js/controllers/loginControllers.js
+++ www/js/controllers/loginControllers.js
@@ -8,16 +8,48 @@
 
 
     // 登陆控制器
-    function loginCtrlFun($scope,$state,tipMsg){
+    function loginCtrlFun($scope,$state,$ionicModal,tipMsg){
         $scope.loginData={};
-        $scope.login=loginFun;
+        $scope.login=loginFun;//登陆
+        $scope.toSignUp=toSignUpFun;//弹出注册框
+        $scope.closeSignUp=closeSignUpFun;//关闭注册框
+        $scope.signUp=signUpFun;//注册
 
+
+        // 创建一个弹出窗模板
+        $ionicModal.fromTemplateUrl('templates/models/sign-up.html', {
+            scope: $scope,//继承自父scope
+            animation: 'slide-in-up'//弹出动画
+        }).then(function(modal) {
+            $scope.signUpmodal = modal;
+        });
+
+        //登陆
         function loginFun(){
-            console.log($scope.loginData);
             if($scope.loginData.username == 'samCooker'&&$scope.loginData.password == 1){
-                $state.go('app.newfordos');
+                $state.go('app.newfordos');//调整至新待办页面
             }else{
                 tipMsg.showMsg('username or password error!');
+            }
+        }
+
+        //弹出注册框
+        function toSignUpFun(){
+            $scope.signUpmodal.show();
+        }
+
+        //关闭注册框
+        function closeSignUpFun(){
+            $scope.signUpmodal.hide();
+        }
+
+        //注册
+        function signUpFun(valid){
+            tipMsg.showMsg(valid);
+            if(valid){
+
+            }else{
+                tipMsg.showMsg("注册不成功。");
             }
         }
     }
